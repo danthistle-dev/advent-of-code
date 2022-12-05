d0efd665eff39d18a75b12f207b223936109898f
Dan Thistlethwaite <46713285+danthistle-dev@users.noreply.github.com>
2022-12-05 19:39:44 +1000

    Finished day 1

--- 2022/1-calorie-counting/main.js
+++ 2022/1-calorie-counting/main.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 let calories;
 
 try {
-  var data = fs.readFileSync("1-input.txt", "utf8");
+  var data = fs.readFileSync("input.txt", "utf8");
   calories = data.split("\n").map(Number);
 } catch (e) {
   console.log("Error:", e.stack);
@@ -26,4 +26,17 @@ function groupCalories(calories) {
   return groups;
 }
 
-console.log(groupCalories(calories));
+function sumCalories(calories) {
+  return calories.reduce((acc, calorie) => acc + calorie, 0);
+}
+
+const groups = groupCalories(calories);
+const totalCalories = groups.map(sumCalories);
+const orderedCalories = totalCalories.sort((a, b) => b - a);
+
+console.log("Part one answer: ", orderedCalories[0]);
+
+console.log(
+  "Part two answer: ",
+  orderedCalories[0] + orderedCalories[1] + orderedCalories[2]
+);
